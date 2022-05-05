280367ecae2e862cf96af4ef55e463f38683d60c
Edward Hurme <edwardhurme@gmail.com>
2022-05-05 15:11:38 +0200

    plot accra data

--- R/count_plot.R
+++ R/count_plot.R
@@ -2,4 +2,13 @@
 
 load("C:/Users/Edward/Dropbox/MPI/Eidolon/Data/Count/count_ratio.RData")
 
+install.packages("pacman")
+library(pacman)
 
+p_load(tidyverse, lubridate, ggplot2)
+
+summary(batdf)
+
+accra <- batdf[batdf$Location == "Accra",]
+
+plot(accra$timestamp, accra$Count, type = "l")
